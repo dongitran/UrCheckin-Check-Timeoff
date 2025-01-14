edf37e78862cb8641fe60e71ccf90f1186cd9909
Dong Tran <thiendong.iuh@gmail.com>
2025-01-14 20:51:37 +0700

    clone source from collector

---
+++ message-analyzer/src/models/timeoffMessage.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const timeOffMessageSchema = new mongoose.Schema({
+  userId: {
+    type: Number,
+    required: true,
+  },
+  username: {
+    type: String,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  currentDate: {
+    type: Date,
+    default: Date.now,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("TimeOffMessage", timeOffMessageSchema);
