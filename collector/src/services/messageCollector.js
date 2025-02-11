3bf9a29e82dd2b07642beecf2b46c95f9ef76808
Dong Tran <thiendong.iuh@gmail.com>
2025-02-11 13:33:30 +0700

    update

--- collector/src/services/messageCollector.js
+++ collector/src/services/messageCollector.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const TimeOffMessage = require("../models/TimeOffMessage");
+const TimeOffMessage = require("../models/TimeOffMessage.js");
 const logger = require("../utils/logger");
 const { generateToken } = require("./tokenService");
 
